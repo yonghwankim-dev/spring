666699f5cd7069a1b71406371382697b713afd98
yonghwankim-dev <dragonbead95@naver.com>
2025-05-07 15:24:44 +0900

    init redis_gzip

---
+++ demo/springboot_practice-main/redis_gzip/src/main/java/com/nemo/RedisGzipApplication.java
@@ -0,0 +1,13 @@
+package com.nemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisGzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisGzipApplication.class, args);
+	}
+
+}
---
+++ demo/springboot_practice-main/redis_gzip/src/test/java/com/nemo/RedisGzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisGzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
